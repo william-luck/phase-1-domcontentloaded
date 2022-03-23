5ff283dec23d79cfd64964a0a1f1e5769a310455
William Luck <luck.william.h@gmail.com>
2022-03-23 11:27:53 -0500

    commit tree ce76bd63aabd5b8271a7adfbc5ad9414a6418cc7

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+
+
+// prints to the console after the DOM has loaded. 
+document.addEventListener("DOMContentLoaded", () => {
+    const p = document.getElementById('text')
+    p.textContent = "This is really cool!"
+})
+
+
+// // To demonstrate, this is logged first, even though comes second in the load order. 
+// console.log("Something")
